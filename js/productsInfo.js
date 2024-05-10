1ec436e029d4770eeb4e2495f185aa610aeb3e5f
leehoy99 <96473508+leehoy99@users.noreply.github.com>
2024-05-11 02:11:23 +0900

    Add files via upload

---
+++ js/productsInfo.js
@@ -0,0 +1,11 @@
+// 상품정보들
+// productInfo.js 파일
+
+export const productInfo = [
+    { name: 'dsa', price: 150000, category: 'SUITING' },
+    { name: 'dsada', price: 12000, category: 'SUITING' },
+    { name: 'grewge', price: 100000, category: 'SUITING' },
+    { name: 'dsa d', price: 45968, category: 'SUITING' },
+    { name: 'grwe', price: 21575, category: 'SUITING' }
+];
+
--- js/shop.js
+++ js/shop.js
@@ -1,4 +1,8 @@
 
+// import { productInfo } from './productsInfo.js';
+import productInfo from './productInfo.json';
+
+
 const pageItems = 15;
 
 const generateProducts = (products, category, currentPage) => {
@@ -68,33 +72,71 @@ const urlCategory = urlParams.get('category') || ''; // 기본값 설정
 
 const products = [];
 
-for (let i = 1; i <= 30; i++) {
+// for (let i = 1; i <= 30; i++) {
+//     const price = Math.floor(Math.random() * 10) * 10000 + 100000;
+//     const formattedPrice = price.toLocaleString();
+//     const category = getCategoryById(i); // 수정된 부분
+
+//     const product = {   
+//         name: `demo${i}`,
+//         id: i,
+//         price: formattedPrice,
+//         category: category,
+//         imgUrl: `images/productsImg/products${i}.png`
+//     };
+
+//     products.push(product);
+// }
+
+// function getCategoryById(id) {
+//     const categories = [
+//         'SHOP_ALL', 'NEW_RELEASES', 'SUITING', 'OUTERWEAR', 'KNITWEAR',
+//         'TOPS', 'BOTTOMS', 'LOUNGEWEAR', 'FOOTWEAR', 'ACCESSORIES'
+//     ];
+
+//     // id 값을 categories 배열의 인덱스로 사용하여 해당하는 카테고리를 반환
+//     // 데모 상품 작성을 위한 랜덤 카테고리 할당으로, 실제 상품 반영시에는 71열, getCategoryById에서 개별 카테고리 및 상품정보를 입력해야됨.
+//     const index = (id - 1) % (categories.length - 1); // SHOP ALL 제외
+//     return categories.filter(category => category !== 'SHOP_ALL')[index];
+// }
+
+const productInfo = [
+    { name: 'dsa', price: 150000, category: 'SUITING' },
+    { name: 'dsada', price: 12000, category: 'SUITING' },
+    { name: 'grewge', price: 100000, category: 'SUITING' },
+    { name: 'dsa d', price: 45968, category: 'SUITING' },
+    { name: 'grwe', price: 21575, category: 'SUITING' }
+];
+
+for (let i = 0; i <= productInfo.length - 1; i++) {
+    const id = i + 1;
     const price = Math.floor(Math.random() * 10) * 10000 + 100000;
     const formattedPrice = price.toLocaleString();
-    const category = getCategoryById(i); // 수정된 부분
+    // const category = getCategoryById(i); // 수정된 부분
 
     const product = {   
-        name: `demo${i}`,
-        id: i,
+        name: productInfo[i % productInfo.length].name,
+        id: id,
         price: formattedPrice,
-        category: category,
-        imgUrl: `images/productsImg/products${i}.png`
+        category: productInfo[i % productInfo.length].category,
+        imgUrl: `images/productsImg/products${id}.png`
     };
 
     products.push(product);
 }
+console.log(products)
 
-function getCategoryById(id) {
-    const categories = [
-        'SHOP_ALL', 'NEW_RELEASES', 'SUITING', 'OUTERWEAR', 'KNITWEAR',
-        'TOPS', 'BOTTOMS', 'LOUNGEWEAR', 'FOOTWEAR', 'ACCESSORIES'
-    ];
+// function getCategoryById(id) {
+//     const categories = [
+//         'SHOP_ALL', 'NEW_RELEASES', 'SUITING', 'OUTERWEAR', 'KNITWEAR',
+//         'TOPS', 'BOTTOMS', 'LOUNGEWEAR', 'FOOTWEAR', 'ACCESSORIES'
+//     ];
 
-    // id 값을 categories 배열의 인덱스로 사용하여 해당하는 카테고리를 반환
-    // 데모 상품 작성을 위한 랜덤 카테고리 할당으로, 실제 상품 반영시에는 71열, getCategoryById에서 개별 카테고리 및 상품정보를 입력해야됨.
-    const index = (id - 1) % (categories.length - 1); // SHOP ALL 제외
-    return categories.filter(category => category !== 'SHOP_ALL')[index];
-}
+//     // id 값을 categories 배열의 인덱스로 사용하여 해당하는 카테고리를 반환
+//     // 데모 상품 작성을 위한 랜덤 카테고리 할당으로, 실제 상품 반영시에는 71열, getCategoryById에서 개별 카테고리 및 상품정보를 입력해야됨.
+//     const index = (id - 1) % (categories.length - 1); // SHOP ALL 제외
+//     return categories.filter(category => category !== 'SHOP_ALL')[index];
+// }
 
 // 페이지 초기화 함수
 const clearItems = () => {
